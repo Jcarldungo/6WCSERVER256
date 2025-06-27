31210b20263c1bb22a9ebb9d7437430d2346f29c
Jcarldungo <153219005+Jcarldungo@users.noreply.github.com>
2025-06-27 13:10:17 +0800

    Add files via upload

--- 2169JCPD/PRELIMS/utility/data.js
+++ 2169JCPD/PRELIMS/utility/data.js
@@ -1,4 +1,4 @@
 module.exports = {
     firstName: 'Jann Carl',
     lastName: 'Dungo'
-}
+};
--- 2169JCPD/app.js
+++ 2169JCPD/app.js
@@ -1,7 +1,8 @@
-var myLogModule = require('./Prelims/utility/log.js');
-myLogModule.warning('Warning node not found..');
-myLogModule.error("Error: Node encountered an error.. ");
-myLogModule.info("Node is running...");
+var myLogModule = require('./PRELIMS/utility/log.js');
+
+myLogModule.warning('Warning node not configured..');
+myLogModule.error('Error: Node encountered an error..');
+myLogModule.info('Node running...');
 
 var msg =require("./PRELIMS/utility/message.js");
 console.log(msg);
@@ -9,5 +10,5 @@ console.log(msg);
 var person = require("./PRELIMS/utility/data.js");
 console.log(person.firstName + ' ' + person.lastName);
 
-var msg = require("./PRELIMS/utility/log.js");
-msg('Hello World');
+var msg = required ("./PRELIMS/utility/log.js");
+msg("Hello World");
